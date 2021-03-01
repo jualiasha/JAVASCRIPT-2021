971b6342928da5de47c83af82e24a4103f4c1afa
Matviishyna Julia <s2100159@edu.bc.fi>
2021-03-01 21:36:41 +0200

    add array into table

--- Add String in Table/script.js
+++ Add String in Table/script.js
@@ -1,11 +1,10 @@
 (function () {
   const executeScript = () => {
     var tr = document.createElement("tr");
-    var td = document.createElement("td");
     var table = document.querySelector("table");
     table.appendChild(
       tr
-    ).innerHTML = `<td>test</td><td>test name</td><td>test 999</td>`;
+    ).innerHTML = `<td>test</td><td>test name</td><td>999</td><td>nonStaff</td>`;
   };
   window.addEventListener("DOMContentLoaded", executeScript);
 })();
@@ -40,22 +39,6 @@ const getUsers = () => [
   },
 ];
 
-/* const addUser = (single) => {
-  function SingleStu(studentNo, name, age) {
-    this.studentNo = getUsers().forEach((el) => {
-      return el.studentNo;
-    });
-    this.name = getUsers().forEach((el) => {
-      return el.name;
-    });
-    this.age = getUsers().forEach((el) => {
-      return el.age;
-    });
-  }
-  single = new SingleStu();
-
-  // return getUsers()[1].studentNo;
-}; */
 var i;
 const addUser = (i) => getUsers()[i];
 for (i = 0; i < getUsers().length; i++) {
@@ -71,9 +54,62 @@ const getOldest = () => {
       this.max = addUser(i).age;
       this.studentNo = addUser(i).studentNo;
       this.name = addUser(i).name;
-      /*  return addUser(i).studentNo + addUser(i).name + addUser(i).age; */
     }
   }
   return this.studentNo + " " + this.name + " " + this.max;
 };
 console.log(getOldest());
+/*let's make a table in html */
+const WritetheTable = () => {
+  for (var i = 0; i < getUsers().length; i++) {
+    var tr = document.createElement("tr");
+    var table = document.querySelector("table");
+    if (addUser(i).studentNo > 0) {
+      table.appendChild(tr).innerHTML = `<td>${addUser(i).studentNo}</td> <td>${
+        addUser(i).name
+      }</td> <td>${addUser(i).age}</td> <td>nonStaff</td>`;
+    } else {
+      table.appendChild(tr).innerHTML = `<td>${addUser(i).studentNo}</td> <td>${
+        addUser(i).name
+      }</td> <td>${addUser(i).age}</td> <td>Staff</td>`;
+    }
+  }
+};
+window.addEventListener("DOMContentLoaded", WritetheTable);
+
+/*button-sorting*/
+
+function sortTable() {
+  var table, rows, switching, i, x, y, shouldSwitch;
+  table = document.querySelector("table");
+  switching = true;
+  /*Make a loop that will continue until
+  no switching has been done:*/
+  while (switching) {
+    //start by saying: no switching is done:
+    switching = false;
+    rows = table.rows;
+    /*Loop through all table rows (except the
+    first, which contains table headers):*/
+    for (i = 1; i < rows.length - 1; i++) {
+      //start by saying there should be no switching:
+      shouldSwitch = false;
+      /*Get the two elements you want to compare,
+      one from current row and one from the next:*/
+      x = rows[i].getElementsByTagName("TD")[2];
+      y = rows[i + 1].getElementsByTagName("TD")[2];
+      //check if the two rows should switch place:
+      if (Number(x.innerHTML) > Number(y.innerHTML)) {
+        //if so, mark as a switch and break the loop:
+        shouldSwitch = true;
+        break;
+      }
+    }
+    if (shouldSwitch) {
+      /*If a switch has been marked, make the switch
+      and mark that a switch has been done:*/
+      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+      switching = true;
+    }
+  }
+}
