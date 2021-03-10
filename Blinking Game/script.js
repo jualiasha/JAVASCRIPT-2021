72c111d33f1d0f49de8435c435ae2d7ea57d86c8
Matviishyna Julia <s2100159@edu.bc.fi>
2021-03-10 21:02:55 +0200

    +music+image+style

--- Blinking Game/script.js
+++ Blinking Game/script.js
@@ -2,9 +2,15 @@ let circles = document.querySelectorAll(".circle");
 let delay = 1000;
 let scoredisplay = document.querySelector("#score");
 let overlay = document.getElementById("popup");
+let leveloverlay = document.getElementById("popup-level");
 let gameover = document.getElementById("final");
 let startbutton = document.getElementById("start");
 let stopbutton = document.getElementById("stop");
+let GameoverSound = new sound("game-over.mp3");
+let myMusic = new sound("no-mozart.mp3");
+let levelsound = new sound("level.mp3");
+let musicspeed = 1;
+let level = 1;
 
 let close = document.getElementById("close");
 let score = 0;
@@ -12,46 +18,36 @@ let active = 0;
 let activecounter = 0;
 let timer;
 
-const ClickCircles = () => {
-  circles[0].onclick = function () {
-    clicked(0);
-  };
-  circles[1].onclick = function () {
-    clicked(1);
-  };
-  circles[2].onclick = function () {
-    clicked(2);
-  };
-  circles[3].onclick = function () {
-    clicked(3);
-  };
-
-  const clicked = (i) => {
-    console.log("clicked:", i);
-
-    if (active == i) {
-      score++;
-    } else {
-      endGame();
-    }
-
-    if (score % 3 == 0) {
-      delay = delay - 100;
-    }
-
-    scoredisplay.textContent = ` ${score}`;
-  };
-};
-
 function GetCircle(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
+function sound(src) {
+  this.sound = document.createElement("audio");
+  this.sound.src = src;
+  this.sound.setAttribute("preload", "auto");
+  this.sound.setAttribute("controls", "none");
+  this.sound.style.display = "none";
+  document.body.appendChild(this.sound);
+  this.play = function () {
+    this.sound.play();
+  };
+  this.stop = function () {
+    this.sound.pause();
+  };
+  this.speed = function (i) {
+    this.sound.playbackRate = i;
+  };
+}
+
 const startGame = () => {
-  console.log("Game started");
   startbutton.style.display = "none";
   stopbutton.style.display = "block";
-
+  myMusic.play();
+  myMusic.speed(musicspeed);
+  circles.forEach((el) => {
+    el.classList.add("circle-game");
+  });
   circles[0].onclick = function () {
     clicked(0);
   };
@@ -71,7 +67,7 @@ const startGame = () => {
     return endGame();
   }
   const clicked = (i) => {
-    console.log("clicked:", i);
+    //console.log("clicked:", i);
 
     if (active == i) {
       score++;
@@ -79,11 +75,26 @@ const startGame = () => {
       endGame();
     }
 
-    if (score % 3 == 0) {
-      delay = delay - 50;
+    if (score % 2 == 0) {
+      delay -= 50;
+      myMusic.speed(musicspeed + 0.3);
     }
+    console.log(`delay from f ${delay}`);
 
     scoredisplay.textContent = ` ${score}`;
+
+    if (score % 7 == 0 && active == i) {
+      level++;
+      leveloverlay.style.display = "block";
+      document.getElementById("level").textContent = level;
+
+      levelsound.play();
+      document
+        .getElementById("continue")
+        .addEventListener("click", function () {
+          leveloverlay.style.display = "none";
+        });
+    }
   };
 
   let nextActive = pickNew(active);
@@ -93,7 +104,7 @@ const startGame = () => {
 
   active = nextActive;
 
-  console.log("Active:", active);
+  //console.log("Active:", active);
 
   timer = setTimeout(startGame, delay);
 
@@ -113,30 +124,21 @@ if (activecounter - score > 3) {
 
 const endGame = () => {
   clearTimeout(timer);
-  console.log("Game over");
+
   overlay.style.display = "block";
   if (score == 0) {
     gameover.textContent = `Your score is ${score}, are you sleeping? Catch it!!`;
   } else {
-    gameover.textContent = `Your score is ${score}. Good, but you can catch more!`;
+    gameover.textContent = `Your score is ${score}. Level ${level} Good, but you can catch more!`;
   }
-
+  myMusic.stop();
+  GameoverSound.play();
   active = 5;
 };
 
 const reloadGame = () => {
-  console.log("what?");
   window.location.reload();
 };
-
-function Checkclick() {
-  for (let i = 0; i < 4; i++)
-    if (active == clicked(i)) {
-      console.log("ura");
-    } else {
-      console.log("game over");
-    }
-}
 
 close.addEventListener("click", reloadGame);
 startbutton.addEventListener("click", startGame);
