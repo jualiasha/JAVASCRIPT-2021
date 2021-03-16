bb17b0097c64c6f2b432e04a07725ec6b9935924
Matviishyna Julia <s2100159@edu.bc.fi>
2021-03-16 23:25:02 +0200

    https+celsius sign

--- Weather-App/script.js
+++ Weather-App/script.js
@@ -7,12 +7,12 @@ async function GetWearher(event) {
   let city = await document.getElementById("city").value;
   console.log(city);
   let resp = await fetch(
-    `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=32d270017730f45ec35b1f40125ca352`
+    `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=32d270017730f45ec35b1f40125ca352`
   );
   let json = await resp.json();
   weather = json;
   console.log(weather);
-  result.innerHTML = `<h3>temperature now: ${weather.main.temp}</h3><h3>feels like: ${weather.main.feels_like}</h3><h3>weather: ${weather.weather[0].description}</h3>`;
+  result.innerHTML = `<h3>temperature now: ${weather.main.temp}&#8451;</h3><h3>feels like: ${weather.main.feels_like}&#8451;</h3><h3>weather: ${weather.weather[0].description}</h3>`;
 }
 
 button.addEventListener("click", GetWearher);
